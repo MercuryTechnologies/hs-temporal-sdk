9692c6d85af21f2c86f033714ec3906e71afd9aa
Ian Duncan <ian@iankduncan.com>
2024-02-08 16:16:20 +0100

    Gotta actually export the instance o_o

--- src/Temporal/Client.hs
+++ src/Temporal/Client.hs
@@ -22,6 +22,7 @@ module Temporal.Client
   -- * Workflow Client
     WorkflowClient
   , workflowClient
+  , HasWorkflowClient(..)
   -- * Running Workflows
   , StartWorkflowOptions(..)
   , TimeoutOptions(..)
