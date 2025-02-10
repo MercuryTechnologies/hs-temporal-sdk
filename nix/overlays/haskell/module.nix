1db9944e0091cfced143ade49049c5e57bcf0717
jkachmar <j@mercury.com>
2025-02-10 17:14:20 -0500

    refactor(nix): expose top-level dev overlay
    `overlays.development` applies a composition of `haskellOverlays` for
    each GHC version we want to build/test to their respective package sets.
    
    e.g. applying this overlay means that `pkgs.haskell.packages.ghc98` will
    contain `temporal-sdk` (& friends).

--- nix/overlays/haskell/module.nix
+++ nix/overlays/haskell/module.nix
@@ -1,14 +1,49 @@
+{ self, ... }:
 {
-  # Haskell overlays providing the non-Rust `hs-temporal-sdk` packages.
-  #
-  # NOTE: This can't be included in 'flake.overlays' directly, as we need the
-  # top-level `pkgs` argument for `pkgs.haskell.lib` + some system libraries
-  # that are outside the scope of the Haskell package set.
-  flake.haskellOverlays = {
-    hs-temporal-sdk = import ./hs-temporal-sdk.nix;
-    dependencies = {
-      default = import ./deps.nix;
-      ghc910 = import ./ghc910-deps.nix;
+  flake = {
+    # Haskell overlays providing the non-Rust `hs-temporal-sdk` packages.
+    #
+    # NOTE: This can't be included in 'flake.overlays' directly, as we need the
+    # top-level `pkgs` argument for `pkgs.haskell.lib` + some system libraries
+    # that are outside the scope of the Haskell package set.
+    haskellOverlays = {
+      hs-temporal-sdk = import ./hs-temporal-sdk.nix;
+      dependencies = {
+        default = import ./deps.nix;
+        ghc910 = import ./ghc910-deps.nix;
+      };
+    };
+
+    # A top-level nixpkgs overlay that extends supported GHC package sets with
+    # `hs-temporal-sdk` packages & any dependency modifications required for
+    # development.
+    #
+    # NOTE: This should _not_ be used by downstream consumers, and is primarily
+    # exposed to  support our development & CI environments.
+    overlays.development = final: prev: {
+      haskell = (prev.haskell or { }) // {
+        packages = (prev.haskell.packages or { }) // {
+          ghc96 = prev.haskell.packages.ghc96.extend (
+            prev.lib.composeManyExtensions [
+              (self.haskellOverlays.dependencies.default final)
+              (self.haskellOverlays.hs-temporal-sdk final)
+            ]
+          );
+          ghc98 = prev.haskell.packages.ghc98.extend (
+            prev.lib.composeManyExtensions [
+              (self.haskellOverlays.dependencies.default final)
+              (self.haskellOverlays.hs-temporal-sdk final)
+            ]
+          );
+          ghc910 = prev.haskell.packages.ghc910.extend (
+            prev.lib.composeManyExtensions [
+              (self.haskellOverlays.dependencies.default final)
+              (self.haskellOverlays.dependencies.ghc910 final)
+              (self.haskellOverlays.hs-temporal-sdk final)
+            ]
+          );
+        };
+      };
     };
   };
 }
--- nix/packages/module.nix
+++ nix/packages/module.nix
@@ -5,26 +5,7 @@ let
       inherit system;
       overlays = [
         self.overlays.temporal-bridge
-        (final: prev: {
-          haskell = (prev.haskell or { }) // {
-            packages = (prev.haskell.packages or { }) // {
-              ghc96 = prev.haskell.packages.ghc96.extend (self.haskellOverlays.hs-temporal-sdk final);
-              ghc98 = prev.haskell.packages.ghc98.extend (
-                prev.lib.composeManyExtensions [
-                  (self.haskellOverlays.dependencies.default final)
-                  (self.haskellOverlays.hs-temporal-sdk final)
-                ]
-              );
-              ghc910 = prev.haskell.packages.ghc910.extend (
-                prev.lib.composeManyExtensions [
-                  (self.haskellOverlays.dependencies.default final)
-                  (self.haskellOverlays.dependencies.ghc910 final)
-                  (self.haskellOverlays.hs-temporal-sdk final)
-                ]
-              );
-            };
-          };
-        })
+        self.overlays.development
       ];
     };
 in
@@ -39,7 +20,7 @@ in
       packages = {
         # TODO: factor this out to generate the flake package outputs we want
         # to include in our test matrix.
-        temporal_bridge = pkgs.temporal_bridge;
+        temporal-bridge = pkgs.temporal_bridge;
         temporal-sdk-ghc96 = pkgs.haskell.packages.ghc96.temporal-sdk;
         temporal-sdk-ghc98 = pkgs.haskell.packages.ghc98.temporal-sdk;
         temporal-sdk-ghc910 = pkgs.haskell.packages.ghc910.temporal-sdk;
