1f3162af68960f9a109566f027a03580cd2e8e1d
jkachmar <j@mercury.com>
2025-04-22 17:19:01 -0400

    fix(sdk): start -> initialize workflow rename
    this only handles the rename, it doesn't attempt to use any of the job
    application schemes discussed elsewhere.

--- sdk/src/Temporal/Client.hs
+++ sdk/src/Temporal/Client.hs
@@ -83,14 +83,12 @@ import qualified Control.Monad.Trans.Writer.CPS as CW
 import qualified Control.Monad.Trans.Writer.Lazy as LW
 import qualified Control.Monad.Trans.Writer.Strict as SW
 import Data.Foldable (for_)
-import Data.Int (Int64)
 import Data.Map (Map)
 import Data.Maybe
 import Data.ProtoLens.Field
 import Data.ProtoLens.Message
 import Data.Proxy
 import Data.Text (Text)
-import Data.Time.Clock.System (SystemTime)
 import Data.Typeable
 import qualified Data.UUID as UUID
 import qualified Data.UUID.V4 as UUID
@@ -116,7 +114,7 @@ import Temporal.Client.Types
 import Temporal.Common
 import qualified Temporal.Core.Client as Core
 import Temporal.Core.Client.WorkflowService
-import Temporal.Duration (Duration, durationToProto)
+import Temporal.Duration (durationToProto)
 import Temporal.Exception
 import Temporal.Payload
 import Temporal.SearchAttributes.Internal
@@ -346,7 +344,8 @@ signal (WorkflowHandle _ _t c wf r) (signalRef -> (KnownSignal sName sCodec)) op
         -- & WF.control .~ _
         -- TODO put other useful headers in here
         & WF.header .~ headerToProto (fmap convertToProtoPayload hdrs)
-        & WF.skipGenerateWorkflowTask .~ opts.skipGenerateWorkflowTask
+        -- FIXME: Can we just ignore this now that it's no longer present?
+        -- & WF.skipGenerateWorkflowTask .~ opts.skipGenerateWorkflowTask
   case result of
     Left err -> throwIO err
     Right _ -> pure ()
--- sdk/src/Temporal/WorkflowInstance.hs
+++ sdk/src/Temporal/WorkflowInstance.hs
@@ -22,7 +22,6 @@ import qualified Control.Exception as E
 import Control.Monad
 import Control.Monad.Logger
 import Control.Monad.Reader
-import Data.Foldable
 import Data.Functor.Identity
 import qualified Data.HashMap.Strict as HashMap
 import Data.ProtoLens
@@ -53,7 +52,7 @@ import Proto.Temporal.Sdk.Core.WorkflowActivation.WorkflowActivation (
   ResolveRequestCancelExternalWorkflow,
   ResolveSignalExternalWorkflow,
   SignalWorkflow,
-  StartWorkflow,
+  InitializeWorkflow,
   UpdateRandomSeed,
   WorkflowActivation,
   WorkflowActivationJob,
@@ -92,7 +91,7 @@ create
   -> Vault
   -> PayloadProcessor
   -> Info
-  -> StartWorkflow
+  -> InitializeWorkflow
   -> m WorkflowInstance
 create
   workflowCompleteActivation
@@ -282,19 +281,19 @@ activate act suspension = do
 
 
 -- | This gives us the basic state for a workflow instance prior to initial evaluation.
-setUpWorkflowExecution :: StartWorkflow -> InstanceM ExecuteWorkflowInput
-setUpWorkflowExecution startWorkflow = do
+setUpWorkflowExecution :: InitializeWorkflow -> InstanceM ExecuteWorkflowInput
+setUpWorkflowExecution initializeWorkflow = do
   inst <- ask
   let (WorkflowGenM genRef) = inst.workflowRandomnessSeed
-  writeIORef genRef (mkStdGen $ fromIntegral $ startWorkflow ^. Activation.randomnessSeed)
-  writeIORef inst.workflowTime (startWorkflow ^. Activation.startTime . to timespecFromTimestamp)
+  writeIORef genRef (mkStdGen $ fromIntegral $ initializeWorkflow ^. Activation.randomnessSeed)
+  writeIORef inst.workflowTime (initializeWorkflow ^. Activation.startTime . to timespecFromTimestamp)
   info <- readIORef inst.workflowInstanceInfo
 
   pure $
     ExecuteWorkflowInput
-      { executeWorkflowInputType = startWorkflow ^. Activation.workflowType
-      , executeWorkflowInputArgs = fmap convertFromProtoPayload (startWorkflow ^. Command.vec'arguments)
-      , executeWorkflowInputHeaders = fmap convertFromProtoPayload (startWorkflow ^. Activation.headers)
+      { executeWorkflowInputType = initializeWorkflow ^. Activation.workflowType
+      , executeWorkflowInputArgs = fmap convertFromProtoPayload (initializeWorkflow ^. Command.vec'arguments)
+      , executeWorkflowInputHeaders = fmap convertFromProtoPayload (initializeWorkflow ^. Activation.headers)
       , executeWorkflowInputInfo = info
       }
 
@@ -462,10 +461,12 @@ applyJobs jobs fAwait = UnliftIO.try $ do
             Just (WorkflowActivationJob'UpdateRandomSeed updateRandomSeed) -> (patchNotifications, signalWorkflows, queryWorkflows, resolutions, otherJobs *> applyUpdateRandomSeed updateRandomSeed)
             Just (WorkflowActivationJob'CancelWorkflow cancelWorkflow) -> (patchNotifications, signalWorkflows, queryWorkflows, PendingWorkflowCancellation cancelWorkflow : resolutions, otherJobs)
             -- By the time we get here, the workflow should already be running.
-            Just (WorkflowActivationJob'StartWorkflow _startWorkflow) -> tup
+            Just (WorkflowActivationJob'InitializeWorkflow _initializeWorkflow) -> tup
             -- Handled in the worker.
             Just (WorkflowActivationJob'RemoveFromCache _removeFromCache) -> tup
             Just (WorkflowActivationJob'DoUpdate _) -> error "DoUpdate not yet implemented"
+            Just (WorkflowActivationJob'ResolveNexusOperation _) -> error "ResolveNexusOperation not yet implemented"
+            Just (WorkflowActivationJob'ResolveNexusOperationStart _) -> error "ResolveNexusOperationStart not yet implemented"
             Nothing -> E.throw $ RuntimeError "Uncrecognized workflow activation job variant"
         )
         (pure (), [], pure (), [], pure ())
