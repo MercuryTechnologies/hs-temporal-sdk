a0e2996c49be5ceb7fcf90713057eb2ff190951d
John Mastro <johnmastro@mercury.com>
2025-06-13 09:41:06 -0400

    Reapply "say that it's a temporal error"
    This reverts commit 103db16947f934f9b3ea9942b8d014322b5f1b3b.

--- core/rust/src/client.rs
+++ core/rust/src/client.rs
@@ -211,7 +211,7 @@ pub fn connect_client(
                     runtime,
                 }),
                 Err(e) => {
-                    eprintln!("Error: {:?}", e);
+                    eprintln!("Error connecting to Temporal server: {:?}", e);
                     let err_message = e.to_string().into_bytes();
                     Err(CArray::c_repr_of(err_message).unwrap())
                 }
