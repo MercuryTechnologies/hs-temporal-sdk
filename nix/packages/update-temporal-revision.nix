bc10d1f14b777474b6c06390f228a56c1b7b3932
Ian Duncan <ian@iankduncan.com>
2025-07-21 18:23:04 +0200

    See if we can get PROTOC set properly for GHA (#153)

--- nix/packages/update-temporal-revision.nix
+++ nix/packages/update-temporal-revision.nix
@@ -3,4 +3,8 @@ pkgs.writeShellApplication {
   name = "update-temporal-revision";
   runtimeInputs = with pkgs; [bash curl jq yq cargo crate2nix];
   text = builtins.readFile ../../scripts/update-temporal-revision.sh;
+  # Export PROTOC environment variable
+  runtimeEnv = {
+    PROTOC = "${pkgs.protobuf}/bin/protoc";
+  };
 }
